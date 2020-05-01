ef981f1818c296aa56fb60f093771fb87ca155a7
tombetthauser <tombetthauser@gmail.com>
2020-05-01 15:51:53 -0700

    Add functional suggestion option

--- app1.py
+++ app1.py
@@ -1,15 +1,32 @@
-import json
+from json import load
+from difflib import get_close_matches
+from random import random
 
-data = json.load(open("data.json"))
+data = load(open("data.json"))
 words = list(data.keys())
 
+yesTup = ('yes', 'y', 'ok')
+noTup = ('no', 'n', 'nope')
+
 def translate(word):
+  word = word.lower()
+
   if word in data:
     return data[word]
+
+  matches = get_close_matches(word, data.keys())
+  matchLen = len(matches)
+  if matchLen > 0:
+    randomIdx = int(random() * matchLen)
+    suggestion = matches[randomIdx]
+    text = "That's almost a word... Did you mean %s instead? " % suggestion
+    yn = input(text)
+    if yn.lower() in yesTup:
+      return ["Ok, here's the definition for %s." % suggestion] + data[suggestion]
   else:
     return ["That word is not in this dictionary. Please double-check you're spelling and try again."]
 
-word = input("Enter a word:")
+word = input("Please enter a word: ")
 translation = translate(word)
 
 for entry in translation:
